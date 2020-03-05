c4d03a34b836acf9ffdb28746f26d56ec678072c
Rodrigo Prestes Machado <rodrigo.prestes@poa.ifrs.edu.br>
2020-03-05 20:03:27 -0300

    teste linux

--- ajax/ex1/resposta.php
+++ ajax/ex1/resposta.php
@@ -12,7 +12,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */
+ */ 
 <?php
     $strSearch = $_GET["name"];
     $number = $_GET["number"];
