93087b775f590c45497e6813ebf044df2cb578b9
Rodrigo Prestes Machado <rodrigo.prestes@gmail.com>
2025-09-15 21:35:32 -0300

    update slots

---
+++ exemplos/vue-slots/js/UserCard.js
@@ -0,0 +1,21 @@
+export default {
+  name: "UserComponent",
+  props: {
+    user: {
+      type: String,
+      required: true,
+    },
+  },
+  template: `
+    <div class="card mb-4">
+        <div class="card-content">
+            <slot :user="user" :isOnline="isUserOnline"></slot>
+        </div>
+    </div>
+  `,
+  computed: {
+    isUserOnline() {
+      return Math.random() > 0.5; // Simula status online/offline
+    },
+  },
+};
